d704b7fa74e55890a4569da390cfdfcb3ab65820
jr <jr@jrs-MacBook-Air-3.local>
2015-05-07 12:25:39 -0400

    added more apps

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 brew install caskroom/cask/brew-cask && brew cask install google-chrome && brew
 cask install virtualbox && brew cask install iterm2 && brew installcask shiftit
 && brew cask install bettertouchtool && brew cask install cyberduck && brew cask
-install quicksilver && brew cask install vagrant  
+install quicksilver && brew cask install vagrant && brew cask install atom &&
+  brew cask install avast && brew cask install karabiner && brew cask install
+seil && brew cask install jing && brew cask install postgres && brew cask
+install sitesucker && brew cask install sourcetree && brew cask install
+ssh-tunnel-manager && brew cask install vlc && brew cask install teamviewer &&
+  brew cask install pgadmin3
