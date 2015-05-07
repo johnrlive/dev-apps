ce3c145b0364c97bc3b7412cd2121250d6f6b2a1
jr <jr@jrs-MacBook-Air-3.local>
2015-05-07 12:35:21 -0400

    format

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 brew install caskroom/cask/brew-cask && \
 brew cask install google-chrome && \
 brew cask install virtualbox && \
-brew cask install iterm2 && brew installcask shiftit && brew cask install bettertouchtool && brew cask install cyberduck && brew cask install quicksilver && brew cask install vagrant && brew cask install atom &&
-  brew cask install avast && brew cask install karabiner && brew cask install
+brew cask install iterm2
+
+&& brew installcask shiftit && brew cask install bettertouchtool && brew cask
+install cyberduck && brew cask install quicksilver && brew cask install vagrant
+&& brew cask install atom &&  brew cask install avast && brew cask install karabiner && brew cask install
 seil && brew cask install jing && brew cask install postgres && brew cask
 install sitesucker && brew cask install sourcetree && brew cask install
 ssh-tunnel-manager && brew cask install vlc && brew cask install teamviewer &&
