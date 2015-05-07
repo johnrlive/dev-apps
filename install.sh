33b22dc899a99fb8a32ea8414d9d00727449097a
jr <jr@jrs-MacBook-Air-3.local>
2015-05-07 11:57:59 -0400

    first commit

---
+++ install.sh
@@ -0,0 +1,4 @@
+brew install caskroom/cask/brew-cask && brew cask install google-chrome && brew
+cask install virtualbox && brew cask install iterm2 && brew installcask shiftit
+&& brew cask install bettertouchtool && brew cask install cyberduck && brew cask
+install quicksilver && brew cask install vagrant  
