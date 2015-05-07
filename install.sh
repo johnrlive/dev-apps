686f032c7a1e490966cfa092f5759dc6990bd94f
John R Live <john@addlocal.com>
2015-05-07 12:52:57 -0400

    Formated & Categorized Apps

--- install.sh
+++ install.sh
@@ -1,12 +1,36 @@
 brew install caskroom/cask/brew-cask && \
-brew cask install google-chrome && \
-brew cask install virtualbox && \
-brew cask install iterm2
 
-&& brew installcask shiftit && brew cask install bettertouchtool && brew cask
-install cyberduck && brew cask install quicksilver && brew cask install vagrant
-&& brew cask install atom &&  brew cask install avast && brew cask install karabiner && brew cask install
-seil && brew cask install jing && brew cask install postgres && brew cask
-install sitesucker && brew cask install sourcetree && brew cask install
-ssh-tunnel-manager && brew cask install vlc && brew cask install teamviewer &&
-  brew cask install pgadmin3
+# Browsers
+brew cask install google-chrome && \
+brew cask install firefox && \
+
+# Dev Tweaks
+brew install cask shiftit && \
+brew cask install bettertouchtool && \
+brew cask install karabiner && \
+brew cask install seil && \
+
+# Dev Tools
+brew cask install iterm2 && \
+brew cask install cyberduck && \
+brew cask install atom && \
+brew cask install sitesucker && \
+brew cask install ssh-tunnel-manager && \
+brew cask install quicksilver && \
+
+# Dev Apps
+brew cask install vagrant && \
+brew cask install virtualbox && \
+brew cask install sourcetree && \
+
+# Database Apps
+brew cask install postgres && \
+brew cask install pgadmin3 && \
+brew cask install sqlitebrowser && \
+brew cask install mysqlworkbench && \
+
+# Other Apps
+brew cask install jing && \
+brew cask install vlc && \
+brew cask install teamviewer && \
+brew cask install avast && \
